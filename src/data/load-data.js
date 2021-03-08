84eeff196fe6703e7f1ae2cf1120294cb8427535
Tyler Morales <moratyle@gmail.com>
2021-03-07 19:33:52 -0600

    ✏️  Add index value to data

--- src/data/load-data.js
+++ src/data/load-data.js
@@ -5,17 +5,18 @@ async function loadData() {
   data.map(addBalance)
 }
 
-async function addBalance({ month, amount }) {
+async function addBalance({ index, month, amount }) {
   try {
-    const data = { month, amount }
-
+    const data = { index, month, amount }
     // look up movie matching the month and amount
     const snapshot = await db
-      .collection('balance')
-      .where('month', '==', month)
-      .where('amount', '==', amount)
-      .get()
-
+    .collection('balance')
+    .where('index', '==', index)
+    .where('month', '==', month)
+    .where('amount', '==', amount)
+    .get()
+    
+    console.log('🎉 Data Loaded!!!')
     // create a doc reference that points to where this movie is located in the db - either a enw doc if it is not there, or the existing doc
     let docRef
     if (snapshot.empty) {
